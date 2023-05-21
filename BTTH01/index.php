07e0052f3905d6dc87ec9bf84322542e45c8abfc
Tung <dothanhtungnb602@gmail.com>
2023-05-21 14:27:38 +0700

    update

--- BTTH01/index.php
+++ BTTH01/index.php
@@ -1,5 +1,11 @@
 <?php
 include 'includes/header.php';
+include 'includes/validate_form.php'
+?>
+<?php
+if ($message != '') {
+    echo "<script type='text/javascript'>alert('$message');</script>";
+}
 ?>
 <!-- FORM -->
 
@@ -11,20 +17,21 @@ include 'includes/header.php';
     </div>
     <div class="row">
         <div class="col">
-            <form method="POST" action="upload.php" enctype="multipart/form-data">
+            <form method="POST" action="addStudent.php" enctype="multipart/form-data">
                 <div class="mb-3">
                     <label for="id" class="form-label">Id</label>
                     <input type="text" class="form-control" id="id">
-                    <div id="idHelp" class="form-text">id existed</div>
+                    <div id="idHelp" class="form-text"><?= $error['id'] ?></div>
                 </div>
                 <div class="mb-3">
                     <label for="name" class="form-label">Name: </label>
                     <input type="text" class="form-control" id="name">
+                    <div id="nameHelp" class="form-text"><?= $error['name'] ?></div>
                 </div>
                 <div class="mb-3">
                     <label for="age" class="form-label">Age: </label>
                     <input type="text" class="form-control" id="age">
-                    <div id="ageHelp" class="form-text">invalid date</div>
+                    <div id="ageHelp" class="form-text"><?= $error['age'] ?></div>
                 </div>
                 <button type="submit" class="btn btn-primary">Add</button>
             </form>
