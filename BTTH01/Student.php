74885d443205280737e9ee6af9206be4fa95a3a4
Tung <dothanhtungnb602@gmail.com>
2023-05-21 14:23:21 +0700

    update form

--- BTTH01/Student.php
+++ BTTH01/Student.php
@@ -1,31 +1,38 @@
 <?php
-class student{
+class student
+{
     private int $id;
     private string $name;
     private int $age;
 
-    public function __construct($id , $name , $age){
+    public function __construct($id = '', $name = '', $age = '')
+    {
         $this->id = $id;
         $this->name = $name;
         $this->age = $age;
     }
-    function getID(){
+    function getID()
+    {
         return $this->id;
     }
-    function getName(){
+    function getName()
+    {
         return $this->name;
     }
-    function getAge(){
+    function getAge()
+    {
         return $this->age;
     }
-    function setID(int $id){
+    function setID(int $id)
+    {
         $this->id = $id;
     }
-    function setName(string $name){
+    function setName(string $name)
+    {
         $this->name = $name;
     }
-    function setAge(int $age){
+    function setAge(int $age)
+    {
         $this->age = $age;
     }
-} 
-?>
+}
